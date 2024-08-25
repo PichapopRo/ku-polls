39b2dfd315caa2ed1a950cfda38f43f314d0c7a1
Pichapop Rodduayboon <pichapop.ro@ku.th>
2024-08-25 11:57:08 +0700

    Add CSS styling and stylesheet

--- poll-config/polls/views.py
+++ poll-config/polls/views.py
@@ -14,15 +14,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).prderby("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+               :5
+               ]
 
 
-class DetailView(generic.DateDetailView):
+class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
     def get_queryset(self):
-        """ Excludes any questions that aren't published yet"""
+        """
+        Excludes any questions that aren't published yet.
+        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
